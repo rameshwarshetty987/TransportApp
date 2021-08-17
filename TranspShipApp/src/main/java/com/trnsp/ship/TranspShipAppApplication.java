395346f8eeb4a3cef4ca560749b8b0fc322054a2
rameshwar setty <rameshwarshetty987@gmail.com>
2021-08-17 22:20:11 +0530

    Shipment Project Created

---
+++ TranspShipApp/src/main/java/com/trnsp/ship/TranspShipAppApplication.java
@@ -0,0 +1,13 @@
+package com.trnsp.ship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranspShipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranspShipAppApplication.class, args);
+	}
+
+}
---
+++ TranspShipApp/src/test/java/com/trnsp/ship/TranspShipAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trnsp.ship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranspShipAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
