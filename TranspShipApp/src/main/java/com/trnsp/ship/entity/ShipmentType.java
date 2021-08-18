c6569ad4b2946a427267e64d24c9d61e3a3aabd7
rameshwar setty <rameshwarshetty987@gmail.com>
2021-08-18 08:19:07 +0530

    entity class linked with table

--- TranspShipApp/src/main/java/com/trnsp/ship/entity/ShipmentType.java
+++ TranspShipApp/src/main/java/com/trnsp/ship/entity/ShipmentType.java
@@ -1,20 +1,36 @@
 package com.trnsp.ship.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="shipment_tab")
 public class ShipmentType {
 	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
 	private Long id;
 	
+	@Column(name="ship_mode")
 	private String shipmentMode;
 	
+	@Column(name="ship_code")
 	private String shipmentCode;
 	
+	@Column(name="enable_ship")
 	private String enableShipment;
 	
+	@Column(name="ship_grade")
 	private String shipmentGrade;
 	
+	@Column(name="ship_descrp")
 	private String shipmentDescrp;
 
 }
